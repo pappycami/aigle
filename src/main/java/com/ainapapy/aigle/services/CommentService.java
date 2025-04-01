72c5373b46a2a21de5cc03d89cfe159b90d945b7
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-01 12:25:42 +0300

    add - Services

---
+++ src/main/java/com/ainapapy/aigle/services/CommentService.java
@@ -0,0 +1,31 @@
+package com.ainapapy.aigle.services;
+
+import com.ainapapy.aigle.models.Comment;
+import com.ainapapy.aigle.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Optional<Comment> getCommentById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    public Comment saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public void deleteComment(Long id) {
+        commentRepository.deleteById(id);
+    }
+}
