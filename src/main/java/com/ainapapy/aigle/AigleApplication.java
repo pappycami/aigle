9a17b8a55989de4da740fbc1d3f6cf69033ddc39
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-01 09:43:55 +0300

    init project - Aigle

---
+++ src/main/java/com/ainapapy/aigle/AigleApplication.java
@@ -0,0 +1,13 @@
+package com.ainapapy.aigle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AigleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ainapapy/aigle/AigleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainapapy.aigle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
