56f46655a855d8c1ee40010e60d5735be3446bbb
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-03 15:50:37 +0300

    add - Style scripts and Jquery

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,20 @@
+(function($){
+    $(document).ready(function () {
+        $.fn.resizeiframe = function () {
+            $(this).load(function () {
+                $(this).height($(this).contents().find("body").height());
+            });
+
+            $(this).click(function () {
+                $(this).height($(this).contents().find("body").height());
+            });
+        };
+        $('iframe').resizeiframe();
+    });
+    
+    $(document).ready(() => {
+        $(".btn").on('click', function(){
+            alert("bouton clicker!!!");
+        });
+    });
+})(jQuery);
